b3deb4199f5e249bdb00c5764e12d0df64b25932
HarlandLohora <harland@lohora.com>
2021-02-28 15:25:00 -0300

    :construction: microservice feed

--- src/server.ts
+++ src/server.ts
@@ -6,19 +6,17 @@ import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
 import {config} from './config/config';
-import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
   await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
   const app = express();
   const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
-
   app.use(cors({
     allowedHeaders: [
       'Origin', 'X-Requested-With',
@@ -32,7 +30,7 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
   app.use('/api/v0/', IndexRouter);
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
+  app.get( '/', async ( req: express.Request, res: express.Response ) => {
     res.send( '/api/v0/' );
   } );
 
